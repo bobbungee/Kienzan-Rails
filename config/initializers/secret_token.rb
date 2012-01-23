b6d62ac1e38c36a5a216e7812a2df02a2a6c3b4d
Rishi Shah <bobbungee@gmail.com>
2012-01-23 16:35:29 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kienzan::Application.config.secret_token = '7a8f2339d30e09484d75a9dab01ec96a48184deab4ab62e594c8a5cbe1b48903feda27586e050656fe3741784a49066e404734ce0b5e1eafb473c0a7be8bb384'
